e721ad8f9263a2da5842cb7b3bdcf8971af7bb1a
Denis Obada <denis.o@linux.com>
2021-09-04 21:13:21 +0300

    Access environment variables

---
+++ env/env.go
@@ -0,0 +1,19 @@
+package env
+
+import "os"
+
+// Env - Fetch environment variable
+func Env(name string, defaultValue string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		return defaultValue
+	}
+
+	return v
+}
+
+// IsDefined - check if environment variable is defined
+func IsDefined(name string) bool {
+	v := os.Getenv(name)
+	return v == ""
+}
