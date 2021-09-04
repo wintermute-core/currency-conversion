aa5cb123459240ba8a53d23bc6c75195b1ecf164
Denis Obada <denis.o@linux.com>
2021-09-04 22:43:37 +0300

    Background job for currency sync

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jasonlvhit/gocron v0.0.1 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"github.com/denis256/currency-conversion/env"
 	"github.com/denis256/currency-conversion/http"
 	"github.com/denis256/currency-conversion/sync"
+	"github.com/jasonlvhit/gocron"
+	"log"
+	"strconv"
 )
 
 func main() {
@@ -11,9 +15,29 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	go executeSyncJob()
 
 	err = http.Server()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func syncCurrenciesInBackground() {
+	err := sync.FetchCurrencies()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func executeSyncJob() {
+	syncInterval, err := strconv.Atoi(env.Env("SYNC_INTERVAL_MIN", "60"))
+	if err != nil {
+		panic(err)
+	}
+	err = gocron.Every(uint64(syncInterval)).Minutes().Do(syncCurrenciesInBackground)
+	if err != nil {
+		panic(err)
+	}
+	<-gocron.Start()
 }
