88cd0810b9f3f29d4693fa36ed4803ad6732f870
Denis Obada <denis.o@linux.com>
2021-09-04 21:03:35 +0300

    Basic fixer api client

---
+++ api/fixer/fixer.go
@@ -0,0 +1,62 @@
+package fixer
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Request - parameters to do request to Fixer API
+type Request struct {
+	ApiKey  string
+	Timeout time.Duration
+}
+
+// Response - Fixer API response
+type Response struct {
+	Success bool               `json:"success"`
+	Base    string             `json:"base"`
+	Date    string             `json:"date"`
+	Rates   map[string]float64 `json:"rates"`
+}
+
+func (fixer Request) FetchExchangeRate() (*Response, error) {
+
+	url := fmt.Sprintf("http://data.fixer.io/api/latest?access_key=%s&format=1", fixer.ApiKey)
+	spaceClient := http.Client{
+		Timeout: fixer.Timeout,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		return nil, getErr
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	data, err := parseResponse(body)
+	return data, err
+
+}
+
+func parseResponse(body []byte) (*Response, error) {
+	data := Response{}
+	jsonErr := json.Unmarshal(body, &data)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+	return &data, nil
+}
---
+++ api/fixer/fixer_test.go
@@ -0,0 +1,23 @@
+package fixer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+
+	"testing"
+)
+
+func TestParseFixerResponse(t *testing.T) {
+
+	buf, err := ioutil.ReadFile("../../docs/fixer-io-example.json")
+	if err != nil {
+		panic(err)
+	}
+	response, err := parseResponse(buf)
+	if err != nil {
+		panic(err)
+	}
+	assert.True(t, response.Success)
+	assert.Equal(t, "EUR", response.Base)
+	assert.Equal(t, 1.18835, response.Rates["USD"])
+}
